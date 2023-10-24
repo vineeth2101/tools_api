a60df63ac66278e644a09832f6a02c898c3e845c
vineeth2101 <viswanathanvineeth01@gmail.com>
2023-10-24 20:56:18 +0530

    conftest.html updated

--- conftest.py
+++ conftest.py
@@ -5,6 +5,7 @@ import allure
 # Initialize the data_dict fixture to manage request data
 request_data_dict = {"headers": {}, "params": {}, "cookies": {}}
 
+
 def pytest_bdd_before_scenario(request, feature, scenario):
     # Clear headers, params, and cookies before each scenario
 
@@ -19,6 +20,6 @@ def pytest_bdd_after_scenario(request, feature, scenario):
 
 @pytest.fixture(scope="function", autouse=True)
 def data_dict():
-    return request_data_dict
+    return request_data_dict                                 
 
     
